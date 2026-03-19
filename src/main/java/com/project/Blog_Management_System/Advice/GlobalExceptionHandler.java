5091a7bae9ab8f0692b52ed1bc890f09c13ca493
Subhojit Guin <subhojitguin2004@gmail.com>
2026-03-19 19:40:58 +0530

    Add exception handling for unsupported media types in GlobalExceptionHandler

--- src/main/java/com/project/Blog_Management_System/Advice/GlobalExceptionHandler.java
+++ src/main/java/com/project/Blog_Management_System/Advice/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -86,6 +87,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<?>> handleAccessDeniedException(AccessDeniedException ex) {
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.FORBIDDEN)
+                .message(ex.getMessage())
+                .build();
+        return buildErrorResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ApiResponse<?>> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException ex) {
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
                 .message(ex.getMessage())
                 .build();
         return buildErrorResponseEntity(apiError);
