f6149553afd49fc2dc93b3fc6fc3ca5b12fa1b08
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-04-07 00:16:36 +0530

    Add CORS configuration and frontend URL to application properties
    Co-authored-by: Subhojit Guin <subhojitguin2004@gmail.com>

--- src/main/java/com/project/Blog_Management_System/BlogManagementSystemApplication.java
+++ src/main/java/com/project/Blog_Management_System/BlogManagementSystemApplication.java
@@ -1,15 +1,33 @@
 package com.project.Blog_Management_System;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
 public class BlogManagementSystemApplication {
 
+	@Value("${frontend.url}")
+	private String frontendUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlogManagementSystemApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins(frontendUrl);
+			}
+		};
 	}
 
 }
