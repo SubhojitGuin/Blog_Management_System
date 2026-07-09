928ce1757682707713f82e51fc1f7879228ac778
Subhojit Guin <subhojitguin2004@gmail.com>
2026-07-10 00:18:50 +0530

    Add more integration tests for PostController.
    Co-authored-by: Sudeshna Pathak <pathaksudeshna92@gmail.com>

--- src/integration-test/java/com/project/Blog_Management_System/CategoryControllerIT.java
+++ src/integration-test/java/com/project/Blog_Management_System/CategoryControllerIT.java
@@ -1,25 +1,29 @@
 package com.project.Blog_Management_System;
 
+import com.project.Blog_Management_System.Advice.ApiError;
 import com.project.Blog_Management_System.Annotations.WithMockBlogUser;
 import com.project.Blog_Management_System.Constants.ApiRoutes;
+import com.project.Blog_Management_System.Dto.CategoryResponseDTO;
+import com.project.Blog_Management_System.Dto.PostResponseDTO;
 import com.project.Blog_Management_System.Entities.CategoryEntity;
 import com.project.Blog_Management_System.Entities.PostEntity;
 import com.project.Blog_Management_System.Entities.UserEntity;
 import com.project.Blog_Management_System.Repositories.CategoryRepository;
 import com.project.Blog_Management_System.Repositories.PostRepository;
 import com.project.Blog_Management_System.Repositories.UserRepository;
+import com.project.Blog_Management_System.Utils.TestSliceResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.List;
 import java.util.UUID;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class CategoryControllerIT extends BaseIT {
@@ -46,7 +50,7 @@ public class CategoryControllerIT extends BaseIT {
 
     @Nested
     @DisplayName("GET " + ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS)
-    class GetPostsByCategoryTests {
+    class GetPostsByCategory {
 
         private PostEntity post;
 
@@ -59,26 +63,37 @@ public class CategoryControllerIT extends BaseIT {
         @DisplayName("Should return 200 and posts for a valid category")
         @WithMockBlogUser(USERNAME)
         void shouldReturnPostsForValidCategory() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, category.getSlug(), category.getId())
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, category.getSlug(), category.getId())
                             .param("size", "10"))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.content", hasSize(1)))
-                    .andExpect(jsonPath("$.data.content[0].title", is(post.getTitle())))
-                    .andExpect(jsonPath("$.data.content[0].category.id", is(category.getId().toString())))
-                    .andExpect(jsonPath("$.data.empty", is(false)))
-                    .andExpect(jsonPath("$.data.first", is(true)))
-                    .andExpect(jsonPath("$.data.last", is(true)))
-                    .andExpect(jsonPath("$.data.numberOfElements", is(1)));
+                    .andReturn();
+
+            TestSliceResponse<PostResponseDTO> sliceResponse = testResponseExtractor.extractSlicePayload(response, PostResponseDTO.class);
+
+            assertThat(sliceResponse.getContent())
+                    .hasSize(1)
+                    .satisfies(posts -> {
+                        assertThat(posts.getFirst().getTitle()).isEqualTo(post.getTitle());
+                        assertThat(posts.getFirst().getCategory().getId()).isEqualTo(category.getId());
+                    });
+            assertThat(sliceResponse.isEmpty()).isFalse();
+            assertThat(sliceResponse.isFirst()).isTrue();
+            assertThat(sliceResponse.isLast()).isTrue();
+            assertThat(sliceResponse.getNumberOfElements()).isEqualTo(1);
         }
 
         @Test
         @DisplayName("Should return 404 for a non-existent category")
         @WithMockBlogUser(USERNAME)
         void shouldReturn404ForNonExistentCategory() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, "non-existent-slug", UUID.randomUUID())
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, "non-existent-slug", UUID.randomUUID())
                             .param("size", "10"))
                     .andExpect(status().isNotFound())
-                    .andExpect(jsonPath("$.error.message", is(messageService.get("exception.resource.not_found", "Category"))));
+                    .andReturn();
+
+            ApiError errorResponse = testResponseExtractor.extractError(response);
+
+            assertThat(errorResponse.getMessage()).isEqualTo(messageService.get("exception.resource.not_found", "Category"));
         }
 
         @Test
@@ -90,24 +105,34 @@ public class CategoryControllerIT extends BaseIT {
                     .description("This is a test category 1")
                     .build());
 
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, category.getSlug(), category.getId())
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, category.getSlug(), category.getId())
                             .param("size", "10"))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.empty", is(true)))
-                    .andExpect(jsonPath("$.data.first", is(true)))
-                    .andExpect(jsonPath("$.data.last", is(true)))
-                    .andExpect(jsonPath("$.data.numberOfElements", is(0)))
-                    .andExpect(jsonPath("$.data.content", hasSize(0)));
+                    .andReturn();
+
+            TestSliceResponse<PostResponseDTO> sliceResponse = testResponseExtractor.extractSlicePayload(response, PostResponseDTO.class);
+
+            assertThat(sliceResponse.getContent())
+                    .hasSize(0);
+
+            assertThat(sliceResponse.isEmpty()).isTrue();
+            assertThat(sliceResponse.isFirst()).isTrue();
+            assertThat(sliceResponse.isLast()).isTrue();
+            assertThat(sliceResponse.getNumberOfElements()).isEqualTo(0);
         }
 
         @Test
         @DisplayName("Should return 404 when the slug and id do not belong to the same category")
         @WithMockBlogUser(USERNAME)
         void  shouldReturn404WhenTheSlugAndIdBelongToTheSameCategory() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, "non-existent-slug", category.getId())
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, "non-existent-slug", category.getId())
                             .param("size", "10"))
                     .andExpect(status().isNotFound())
-                    .andExpect(jsonPath("$.error.message", is(messageService.get("exception.resource.not_found", "Category"))));
+                    .andReturn();
+
+            ApiError errorResponse = testResponseExtractor.extractError(response);
+
+            assertThat(errorResponse.getMessage()).isEqualTo(messageService.get("exception.resource.not_found", "Category"));
         }
 
         @Test
@@ -117,26 +142,37 @@ public class CategoryControllerIT extends BaseIT {
             PostEntity post2 = postRepository.saveAndFlush(testDataFactory.createCustomPost(user, category));
             PostEntity post3 = postRepository.saveAndFlush(testDataFactory.createCustomPost(user, category));
 
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, category.getSlug(), category.getId())
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, category.getSlug(), category.getId())
                             .param("size", "1")
                             .param("post_cursor", post3.getId().toString()))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.content", hasSize(1)))
-                    .andExpect(jsonPath("$.data.content[0].id", is(post2.getId().toString())))
-                    .andExpect(jsonPath("$.data.content[0].category.id", is(category.getId().toString())))
-                    .andExpect(jsonPath("$.data.empty", is(false)))
-                    .andExpect(jsonPath("$.data.first", is(true)))
-                    .andExpect(jsonPath("$.data.last", is(false)))
-                    .andExpect(jsonPath("$.data.numberOfElements", is(1)));
+                    .andReturn();
+
+            TestSliceResponse<PostResponseDTO> sliceResponse = testResponseExtractor.extractSlicePayload(response, PostResponseDTO.class);
+
+            assertThat(sliceResponse.getContent())
+                    .hasSize(1)
+                    .satisfies(posts -> {
+                        assertThat(posts.getFirst().getId()).isEqualTo(post2.getId());
+                        assertThat(posts.getFirst().getCategory().getId()).isEqualTo(category.getId());
+                    });
+            assertThat(sliceResponse.isEmpty()).isFalse();
+            assertThat(sliceResponse.isFirst()).isTrue();
+            assertThat(sliceResponse.isLast()).isFalse();
+            assertThat(sliceResponse.getNumberOfElements()).isEqualTo(1);
         }
 
         @Test
         @DisplayName("Should return 401 for unauthenticated user")
         void shouldReturn401ForUnauthenticatedUser() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, category.getSlug(), category.getId())
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_POSTS, category.getSlug(), category.getId())
                             .param("size", "10"))
                     .andExpect(status().isUnauthorized())
-                    .andExpect(jsonPath("$.error.message", is("Full authentication is required to access this resource")));
+                    .andReturn();
+
+            ApiError errorResponse = testResponseExtractor.extractError(response);
+
+            assertThat(errorResponse.getMessage()).isEqualTo("Full authentication is required to access this resource");
         }
     }
 
@@ -148,21 +184,30 @@ public class CategoryControllerIT extends BaseIT {
         @DisplayName("Should return 200 and all categories")
         @WithMockBlogUser(USERNAME)
         void shouldReturnAllCategories() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH))
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data", hasSize(2)))
-                    .andExpect(jsonPath("$.data[1].id", is(category.getId().toString())))
-                    .andExpect(jsonPath("$.data[1].name", is(category.getName())))
-                    .andExpect(jsonPath("$.data[1].slug", is(category.getSlug())))
-                    .andExpect(jsonPath("$.data[1].description", is(category.getDescription())));
+                    .andReturn();
+
+            List<CategoryResponseDTO> categoriesList = testResponseExtractor.extractListPayload(response, CategoryResponseDTO.class);
+            assertThat(categoriesList)
+                    .hasSize(2)
+                    .satisfies(categories -> {
+                        assertThat(categories.get(1).getId()).isEqualTo(category.getId());
+                        assertThat(categories.get(1).getName()).isEqualTo(category.getName());
+                        assertThat(categories.get(1).getSlug()).isEqualTo(category.getSlug());
+                        assertThat(categories.get(1).getDescription()).isEqualTo(category.getDescription());
+                    });
         }
 
         @Test
         @DisplayName("Should return 401 for unauthenticated user")
         void shouldReturn401ForUnauthenticatedUser() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH))
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH))
                     .andExpect(status().isUnauthorized())
-                    .andExpect(jsonPath("$.error.message", is("Full authentication is required to access this resource")));
+                    .andReturn();
+
+            ApiError errorResponse = testResponseExtractor.extractError(response);
+            assertThat(errorResponse.getMessage()).isEqualTo("Full authentication is required to access this resource");
         }
     }
 
@@ -174,38 +219,50 @@ public class CategoryControllerIT extends BaseIT {
         @DisplayName("Should return 200 and category details for a valid category")
         @WithMockBlogUser(USERNAME)
         void shouldReturnCategoryDetailsForValidCategory() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_PATH_VARIABLE, category.getSlug(), category.getId()))
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_PATH_VARIABLE, category.getSlug(), category.getId()))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.id", is(category.getId().toString())))
-                    .andExpect(jsonPath("$.data.name", is(category.getName())))
-                    .andExpect(jsonPath("$.data.slug", is(category.getSlug())))
-                    .andExpect(jsonPath("$.data.description", is(category.getDescription())));
+                    .andReturn();
+
+            CategoryResponseDTO categoryResponseDTO = testResponseExtractor.extractPayload(response, CategoryResponseDTO.class);
+            assertThat(categoryResponseDTO.getId()).isEqualTo(category.getId());
+            assertThat(categoryResponseDTO.getName()).isEqualTo(category.getName());
+            assertThat(categoryResponseDTO.getDescription()).isEqualTo(category.getDescription());
+            assertThat(categoryResponseDTO.getSlug()).isEqualTo(category.getSlug());
         }
 
         @Test
         @DisplayName("Should return 404 for a non-existent category")
         @WithMockBlogUser(USERNAME)
         void shouldReturn404ForNonExistentCategory() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_PATH_VARIABLE, "non-existent-slug", UUID.randomUUID()))
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_PATH_VARIABLE, "non-existent-slug", UUID.randomUUID()))
                     .andExpect(status().isNotFound())
-                    .andExpect(jsonPath("$.error.message", is(messageService.get("exception.resource.not_found", "Category"))));
+                    .andReturn();
+
+            ApiError errorResponse = testResponseExtractor.extractError(response);
+            assertThat(errorResponse.getMessage()).isEqualTo(messageService.get("exception.resource.not_found", "Category"));
         }
 
         @Test
         @DisplayName("Should return 404 when the slug and id do not belong to the same category")
         @WithMockBlogUser(USERNAME)
         void shouldReturn404WhenTheSlugAndIdBelongToTheSameCategory() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_PATH_VARIABLE, "non-existent-slug", category.getId()))
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_PATH_VARIABLE, "non-existent-slug", category.getId()))
                     .andExpect(status().isNotFound())
-                    .andExpect(jsonPath("$.error.message", is(messageService.get("exception.resource.not_found", "Category"))));
+                    .andReturn();
+
+            ApiError errorResponse = testResponseExtractor.extractError(response);
+            assertThat(errorResponse.getMessage()).isEqualTo(messageService.get("exception.resource.not_found", "Category"));
         }
 
         @Test
         @DisplayName("Should return 401 for unauthenticated user")
         void shouldReturn401ForUnauthenticatedUser() throws Exception {
-            mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_PATH_VARIABLE, category.getSlug(), category.getId()))
+            MvcResult response = mockMvc.perform(get(ApiRoutes.CATEGORY_BASE_PATH + ApiRoutes.CATEGORY_PATH_VARIABLE, category.getSlug(), category.getId()))
                     .andExpect(status().isUnauthorized())
-                    .andExpect(jsonPath("$.error.message", is("Full authentication is required to access this resource")));
+                    .andReturn();
+
+            ApiError errorResponse = testResponseExtractor.extractError(response);
+            assertThat(errorResponse.getMessage()).isEqualTo("Full authentication is required to access this resource");
         }
     }
 }
